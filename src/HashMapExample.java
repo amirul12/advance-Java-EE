93e51d15b04a193058c1552dcf1bd2a659d9d1a0
Amirul Islam <amirul.csejust@gmail.com>
2016-08-20 00:28:34 +0600

    HashMap example added

---
+++ src/HashMapExample.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HashMapExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<String, String>map = new HashMap<>();
+		 
+		 //put element to the map
+		 map.put("one", "Amirul");
+		 map.put("two", "Mizan");
+		 map.put("three", "Shadhin");
+		 
+		 //size of map
+		 System.out.println("Display the first value: " +map.size());
+		 
+		 System.out.println("Display the first value: " +map.get("one"));
+		 
+		 //show all value that in map
+		 System.out.println("Show all value: " +map.values());
+		 
+		 
+		 
+		 
+	}
+		
+	}
